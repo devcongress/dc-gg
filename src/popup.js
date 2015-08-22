bdd4d898c07b178787097e92b5992a1545fa6b6c
Michael E. Turkson <mturkson23@gmail.com>
2015-08-22 07:49:17 +0000

    fixed document.write errors

--- src/popup.js
+++ src/popup.js
@@ -1,23 +1,29 @@
-    google.load("feeds", "1");
+google.load("feeds", "1");
 
-    function initialize() {
-      var feed = new google.feeds.Feed("https://groups.google.com/forum/feed/devcongress/topics/atom.xml?num=15");
-      feed.setNumEntries(10);
-      var count = 1;
-      feed.load(function(result) {
+function initialize() {
+
+    var feed = new google.feeds.Feed("https://groups.google.com/forum/feed/devcongress/topics/atom.xml?num=15");
+
+    feed.setNumEntries(10);
+
+    var count = 1;
+
+    feed.load(function (result) {
         if (!result.error) {
-          var container = document.getElementById("inner");
-          var html = "";
-          for (var i = 0; i < result.feed.entries.length; i++) {
-            var entry = result.feed.entries[i];
-            topic = "<h5>" + count++ + ". <a href='" + entry.link + "'>" + entry.title + "</a></h5><div id=\"name\">Post by: " + entry.author + "</div>";
-            var div = document.createElement("div");
-	    div.innerHTML = topic;
-            container.appendChild(div);            
-          }
-          document.write(html);
-        }
-      });
-    }
-    google.setOnLoadCallback(initialize);
+            var container = document.getElementById("inner");
+            var html = "";
+            
+            for (var i = 0; i < result.feed.entries.length; i++) {
+                var entry = result.feed.entries[i];
+                topic = "<h5>" + count++ +". <a href='" + entry.link + "'>" + entry.title + "</a></h5><div id=\"name\">Post by: " + entry.author + "</div>";
+                var div = document.createElement("div");
+                
+                div.innerHTML = topic;
 
+                container.appendChild(div);
+            }
+        }
+    });
+}
+
+google.setOnLoadCallback(initialize);
